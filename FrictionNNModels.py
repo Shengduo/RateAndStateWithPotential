c560aa12af4f157f21d66586c5ef80434f9c64d9
Shengduo Liu <shengduoliu@ShengduodeMacBook-Pro.local>
2024-03-11 15:23:17 -0700

    shit.

--- FrictionNNModels.py
+++ FrictionNNModels.py
@@ -168,6 +168,137 @@ class PotentialsFricCorrection:
         self.D.module.to(mapDevice)
         self.D_dagger.module.to(mapDevice)
 
+# Class for Polynomial functions
+class PN(nn.Module):
+    def __init__(self, ProdOrder):
+        super().__init__()
+        self.ProdOrder = ProdOrder
+        self.coefs = nn.Parameter(torch.rand(self.ProdOrder))
+    
+    def getPolyVal(self, x, coeffs):
+        curVal = torch.zeros(x.shape)
+        for curValIndex in range(len(coeffs)-1):
+            curVal = (curVal + coeffs[curValIndex]) * x
+        return (curVal + coeffs[-1])
+    
+    def forward(self, x):
+        res = torch.ones(x.shape)
+        for idx in len(self.coefs):
+            res *= self.getPolyVal(x, self.coefs[idx, :])
+        return res
+
+class PotentialsPolyCorrection:
+    # Initialization of W and D
+    def __init__(self, kwgsPolyPot):
+        # self.dim_xi = kwgsPot["dim_xi"]
+        self.dim_xi = 1 # Now only support 1 hidden variable
+        self.p_order = kwgsPolyPot['p_order']
+        self.coefs_W = PN([1, self.p_order])
+        self.coefs_D = PN([self.dim_xi, self.p_order])
+        self.coefs_D_dagger = PN([1 + self.dim_xi, self.p_order])
+
+        self.optim_W = optim.Adam(self.W.parameters(), lr=kwgsPolyPot["learning_rate"])
+        self.optim_D = optim.Adam(self.D.parameters(), lr=kwgsPolyPot["learning_rate_D"])
+        self.optim_D_dagger = optim.Adam(self.D_dagger.parameters(), lr=kwgsPolyPot["learning_rate_D_dagger"])
+        
+        # Device
+        self.device = kwgsPolyPot["device"]
+        
+        # Multi-GPU data parallel
+        self.W = nn.DataParallel(self.W)
+        self.D = nn.DataParallel(self.D)
+        self.D_dagger = nn.DataParallel(self.D_dagger)
+        
+        self.W.to(self.device)
+        self.D.to(self.device)
+        self.D_dagger.to(self.device)
+        
+    # Calculate f 
+    def calf(self, x, xDot, t):
+        # Initialize Vs
+        batch_size = x.shape[0]
+        time_steps = x.shape[1]
+        # xis[:, :, :] = 1. 
+        
+        
+        # Loop through time steps
+        
+        if self.dim_xi > 0:
+            xiNext = torch.zeros([batch_size, self.dim_xi], requires_grad=True, device=self.device)
+            # xi0 = torch.zeros([batch_size, self.dim_xi], requires_grad=True, device=self.device)
+            
+            # List of fs
+            list_fs = []
+            # list_xis = [xi0]
+            
+            for idx in range(x.shape[1]):
+                # Load xi_curr
+                xiCurr = xiNext
+
+                # f = \partial W / \partial V
+                X_D_dagger = torch.concat([xDot[:, idx:idx + 1], xiCurr], dim = 1).requires_grad_()
+                # X_W.to(self.device)
+                D_dagger = torch.sum(self.D_dagger(X_D_dagger))
+
+                this_piece = torch.autograd.grad(outputs=D_dagger, inputs=X_D_dagger, create_graph=True)[0]
+
+                # Solve for \dot{\xi} + \partial W / \partial \xi = 0
+                dD_daggerdXi = this_piece[:, 1:]
+                dD_daggerdXDot = this_piece[:, 0:1]
+
+                X_W = x[:, idx:idx+1].requires_grad_()
+                W = torch.sum(self.W(X_W))
+                dWdX = torch.autograd.grad(outputs=W, inputs=X_W, create_graph=True)[0]
+
+                list_fs.append(dD_daggerdXDot + dWdX.reshape([-1, 1]))
+
+                # XiDot = dD^*/d\dot{d} (-dD^\dagger / dXi)
+                if idx < x.shape[1] - 1:
+                    this_input = -dD_daggerdXi.clone().requires_grad_()
+                    D = torch.sum(self.D(this_input))
+                    xiNext = xiCurr + torch.autograd.grad(outputs=D, inputs=this_input, create_graph=True)[0] * (t[:, idx + 1:idx + 2] - t[:, idx:idx + 1])
+                    # list_xis.append(xiNext)
+                    
+                    del this_input, dD_daggerdXi, dD_daggerdXDot, W, X_W, dWdX, D, this_piece, X_D_dagger, D_dagger 
+                else:
+                    del dD_daggerdXi, dD_daggerdXDot, W, X_W, dWdX, this_piece, X_D_dagger, D_dagger
+                    
+            self.fs = torch.concat(list_fs, dim=1)
+            del xiNext, xiCurr
+            
+        else:
+            X_W = x.clone().reshape([x.shape[0], x.shape[1], 1]).requires_grad_()
+            # print(X_W)
+            W = torch.sum(self.W(X_W))
+
+            X_D_dagger = xDot.clone().reshape([xDot.shape[0], xDot.shape[1], 1]).requires_grad_()
+            D_dagger = torch.sum(self.D_dagger(X_D_dagger))
+            self.fs = torch.autograd.grad(outputs=W, inputs=X_W, create_graph=True)[0].reshape([x.shape[0], x.shape[1]]) \
+                      + torch.autograd.grad(outputs=D_dagger, inputs=X_D_dagger, create_graph=True)[0].reshape([xDot.shape[0], xDot.shape[1]])
+            del W, X_W, X_D_dagger, D_dagger 
+
+    # Save the model
+    def save(self, PATH):
+        # mkdir
+        Path(PATH).mkdir(parents=True, exist_ok=True)
+
+        # Save all three neural networks
+        torch.save(self, PATH + "/model.pth")
+        torch.save(self.W.module.state_dict(), PATH + "/W.pth")
+        torch.save(self.D.module.state_dict(), PATH + "/D.pth")
+        torch.save(self.D_dagger.module.state_dict(), PATH + "/D_dagger.pth")
+
+    # Load the nns
+    def load_nns(self, PATH, mapDevice):
+        self.W.module.load_state_dict(torch.load(PATH + "/W.pth", map_location=mapDevice))
+        self.D.module.load_state_dict(torch.load(PATH + "/D.pth", map_location=mapDevice))
+        self.D_dagger.module.load_state_dict(torch.load(PATH + "/D_dagger.pth", map_location=mapDevice))
+
+        # Send to devices
+        self.W.module.to(mapDevice)
+        self.D.module.to(mapDevice)
+        self.D_dagger.module.to(mapDevice)
+
 
 class PotsCalXiXiDot:
     # Initialization of W and D
@@ -445,7 +576,7 @@ def load_model(modelPrefix, mapDevice=torch.device("cpu"), dim_xi=1, dict_flag=F
     if dict_flag:
         
         PATH = "./model/" + modelPrefix + "_dimXi_" + str(dim_xi) + "_dict"
-        myModel = torch.load(PATH + "/model.pth")
+        myModel = torch.load(PATH + "/model.pth", map_location = mapDevice)
 
         del myModel.W, myModel.D, myModel.D_dagger
         
@@ -467,6 +598,6 @@ def load_model(modelPrefix, mapDevice=torch.device("cpu"), dim_xi=1, dict_flag=F
         myModel.D = myModel.D.module.to(mapDevice)
         myModel.D_dagger = myModel.D_dagger.module.to(mapDevice)
     else:
-        myModel = torch.load("./model/" + modelPrefix + "_dimXi_" + str(dim_xi) + ".pth")
+        myModel = torch.load("./model/" + modelPrefix + "_dimXi_" + str(dim_xi) + ".pth", map_location = mapDevice)
 
     return myModel
---
+++ TuneDimXi_logV_WDsep_deltaTSqed_combinedSet_PN.py
@@ -0,0 +1,283 @@
+#Ｉmport necessary packages
+import torch
+import torch.nn as nn
+import numpy as np
+from matplotlib import pyplot as plt
+import xitorch
+from xitorch.optimize import rootfinder
+import optuna
+from torch.utils.data import TensorDataset, DataLoader
+import joblib 
+from pathlib import Path
+
+# Memory management on GPU
+import gc
+
+# Import time
+import time
+
+# Testify whether GPU is available
+print("Cuda is available: ", torch.cuda.is_available())
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device = "cpu"
+print("Device is: ", device)
+
+def memory_stats():
+    print("Memory allocated: ", torch.cuda.memory_allocated()/1024**2)
+    print("Memory cached: ", torch.cuda.memory_reserved()/1024**2)
+memory_stats()
+
+## Generating using Burigede's scheme
+from DataGeneration import generateSamples_Burigede
+import os
+generating_flag = False
+kwgs = {
+    "beta" : [0.006, 0.012, 1. / 1.e12, 0.58], 
+    "totalNofSeqs" : 100, # 1024 * 16, 
+    "NofIntervalsRange" : [3, 5], #[5, 11], 
+    "VVRange" : [-3, 1], 
+    "VVLenRange" : [3, 4], 
+    "Tmax" : 2.0, 
+    "nTSteps" : 100, 
+    "theta0" : 0.1, 
+    # "prefix" : "Trial1108_bigDRS_Burigede", 
+    # "prefix" : "Trial1116_smallDRS_largeA", 
+    # "prefix" : "Trial1215_smallDRS_smallA", 
+    # "prefix" : "Trial1215_smallDRS_Burigede", 
+    "prefix" : "Trial0216_combined_800", 
+    "NofVVSteps" : 10, 
+}
+
+# Generate / load data
+dataFile = "./data/" + kwgs["prefix"] + ".pt"
+print("Data file: ", dataFile)
+
+if generating_flag or not(os.path.isfile(dataFile)):
+    print("Generating data")
+    generateSamples_Burigede(kwgs)
+
+shit = torch.load(dataFile)
+Vs = shit["Vs"]
+thetas = shit["thetas"]
+fs = shit["fs"]
+ts = shit["ts"]
+
+# Now Vs and ts have fixed length
+print("Vs.shape: ", Vs.shape)
+print("thetas.shape: ", thetas.shape)
+print("fs.shape: ", fs.shape)
+print("ts.shape: ", ts.shape)
+# Calculate Xs
+Xs = torch.zeros(Vs.shape)
+Xs[:, 1:] = torch.cumulative_trapezoid(Xs, ts)
+print("Xs.shape: ", Xs.shape)
+
+# Different Potentials with D correction
+import torch.optim as optim
+## Calculate f
+# Different Potentials with D correction
+from FrictionNNModels import PotentialsPolyCorrection, Loss, train1Epoch, PN
+
+## Define loss function, training function, dataloaders
+# Initialize dataloaders
+AllData = {
+    "Xs" : Xs, 
+    "Vs" : Vs, 
+    "ts" : ts, 
+    "fs" : fs, 
+}
+
+dataloader_kwargs = {'num_workers': 1, 'pin_memory': True} if torch.cuda.is_available() else {}
+# train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True, **kwargs)
+
+class OptunaObj:
+    # Initialize
+    def __init__(self, kwgs):
+        self.dim_xi = kwgs['dim_xi']
+        self.test_p = kwgs['test_p']
+        self.device = kwgs['device']
+        self.modelSavePrefix = kwgs['modelSavePrefix']
+        self.bestValue = 10000000.
+
+        if 'bestValue' in kwgs.keys():
+            self.bestValue = kwgs['bestValue']
+        
+        self.fOffSet = kwgs['fOffSet']
+        self.scaling_factor = kwgs['scaling_factor']
+
+        # Transform the input data
+        AllData = TensorDataset(
+            kwgs["AllData"]["Xs"], 
+            kwgs["AllData"]["Vs"], 
+            kwgs["AllData"]["ts"], 
+            (kwgs["AllData"]["fs"] - self.fOffSet) * self.scaling_factor + self.fOffSet, 
+        )
+
+        self.dataloader_kwargs = {'num_workers': 1, 'pin_memory': True} if torch.cuda.is_available() else {}
+        # train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True, **kwargs)
+
+        train_len = int(len(Vs) * 0.8)
+        test_len = len(Vs) - train_len
+
+        # Created using indices from 0 to train_size.
+        self.training_dataset = torch.utils.data.Subset(AllData, range(train_len))
+
+        # Created using indices from train_size to train_size + test_size.
+        self.test_dataset = torch.utils.data.Subset(AllData, range(train_len, train_len + test_len))
+
+
+    # Define the objective
+    def objective(self, trial):
+        # Dump for un-saved interuptions
+        # joblib.dump(this_study, "./jobs/" + self.modelSavePrefix + str(self.dim_xi) + ".pkl")
+
+        # Fixed parameters
+        dim_xi = self.dim_xi
+
+        # Suggest learning rate
+        learning_rate = 10 ** trial.suggest_float('log_learning_rate', -5., -1.)
+        
+        # Suggest learning rate for D
+        learning_rate_D = 10 ** trial.suggest_float('log_learning_rate_D', -5., -1.)
+
+        # Suggest learning rate for D
+        learning_rate_D_dagger = 10 ** trial.suggest_float('log_learning_rate_D_dagger', -5., -1.)
+
+        # Suggest batchsize
+        training_batch_size = 2 ** trial.suggest_int('training_batch_size', 6, 12)
+
+        # Suggest training p
+        training_p = trial.suggest_int('training_p', 2, 8)
+
+        # Suggest training epochs
+        # training_epochs = 2 ** trial.suggest_int('training_epoch_exponents', 5, 9)
+        training_epochs = 100
+
+        # Fixing order of polynomial (p_order - 1)
+        p_order = 3
+
+        params = {
+            'dim_xi' : dim_xi, 
+            'p_order' : p_order, 
+            'learning_rate' : learning_rate, 
+            'learning_rate_D' : learning_rate_D, 
+            'learning_rate_D_dagger' : learning_rate_D_dagger,  
+            'training_batch_size' : training_batch_size, 
+            'training_p' : training_p, 
+            'training_epochs' : training_epochs, 
+            'device' : self.device,
+        }
+        
+        
+        # Set training dataloader
+        training_batch_size = params['training_batch_size'] #1024
+        trainDataLoader = DataLoader(
+            self.training_dataset,
+            batch_size = training_batch_size,
+            shuffle = True,
+        #    num_workers = 16,
+            collate_fn = None,
+            **dataloader_kwargs, 
+        )
+
+        # Set testing data loader
+        testing_batch_size = len(self.test_dataset) # 256
+        testDataLoader = DataLoader(
+            self.test_dataset,
+            batch_size = testing_batch_size,
+            shuffle = True,
+        #    num_workers = 16,
+            collate_fn = None,
+            **dataloader_kwargs, 
+        )
+        
+        # Print out info
+        print("-"*20, " Trial ", str(trial.number), " ", "-"*20, flush=True)
+        st = time.time()
+        print("Start timing: ")
+        
+        print("Parameters: ", flush=True)
+        print(trial.params, flush=True)
+        
+        # Training
+        myWD = PotentialsPolyCorrection(params)
+        for i in range(params['training_epochs']):
+            avg_training_loss = train1Epoch(trainDataLoader, Loss, myWD, params['training_p'], 0.)
+            
+            if torch.isnan(avg_training_loss):
+                break
+            
+            if i % 10 == 0:
+                # avg_test_loss = train1Epoch(testDataLoader, Loss, myWD, self.test_p, update_weights=False)
+                print("\t", "epoch ", str(i), "training error: ", str(avg_training_loss), flush=True)
+                ## Print memory status
+                print("Memory status after this epoch: ")
+                memory_stats()
+        
+        # Return objective value for optuna
+        res = train1Epoch(testDataLoader, Loss, myWD, self.test_p, 0., update_weights=False)
+        
+        if res < self.bestValue:
+            print("res: ", res)
+            print("self.bestValue: ", self.bestValue)
+            print("Save this model!")
+
+            saveDir = './model/' + self.modelSavePrefix + '_dimXi_{0}_dict'.format(self.dim_xi)
+            myWD.save(saveDir)
+            self.bestValue = res
+
+        print("Time for this trial: ", time.time() - st)
+        
+        # Release GPU memory
+        del myWD
+        gc.collect()
+        torch.cuda.empty_cache()
+        
+        ## Print memory status
+        print("Memory status after this trial: ")
+        memory_stats()
+        
+        return res
+
+# Do a parametric study over number of hidden parameters
+dim_xis = [1]
+studys = []
+
+# Tune parameters for dim_xi = 4
+OptKwgs = {
+    'dim_xi' : 4, 
+    'test_p' : 2, 
+    # 'test_batch_size' : len(testDataset), 
+    'device' : device, 
+    # 'training_dataset' : trainDataset, 
+    # 'test_dataset' : testDataset, 
+    'modelSavePrefix' : kwgs['prefix'] + "_PN",
+    'fOffSet' : 0.5109, 
+    'scaling_factor' : 50., 
+    'AllData' : AllData, 
+}
+
+# Loop through all dim_xis
+for dim_xi in dim_xis:
+    # sqlite:///example.db
+    this_study = optuna.create_study(direction='minimize', 
+                                     storage="sqlite:///./jobs/{0}_{1}_PN".format(kwgs['prefix'], dim_xi) + ".db", 
+                                     study_name="my_study1", 
+                                     load_if_exists=True)
+
+    OptKwgs['dim_xi'] = dim_xi
+    # print("this_study.best_trial.value: ", this_study.best_trial.value)
+
+    try:
+        OptKwgs['bestValue'] = this_study.best_trial.value
+        print("Pruned database has best value {0}.".format(this_study.best_trial.value))
+    except:
+        OptKwgs['bestValue'] = 10000000.
+        print("No pruned database has been founded.")
+
+
+    myOpt = OptunaObj(OptKwgs)
+
+    this_study.optimize(myOpt.objective, n_trials=200)
+    studys.append(this_study)
