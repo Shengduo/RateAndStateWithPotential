2011a887defcf93225495002e306d2facb7d7e2f
Shengduo <sliu5@caltech.edu>
2024-03-26 10:57:21 -0700

    shit.

--- FrictionNNModels.py
+++ FrictionNNModels.py
@@ -328,6 +328,113 @@ class PotentialsPolyCorrection:
         self.D.module.to(mapDevice)
         self.D_dagger.module.to(mapDevice)
 
+class PotentialsGivenPolyCorrection:
+    # Initialization of W and D
+    def __init__(self, kwgsGivenPoly):
+        # self.dim_xi = kwgsPot["dim_xi"]
+        self.dim_xi = 1 # Now only support 1 hidden variable
+        self.W = kwgsGivenPoly["W"]
+        self.D_dagger = kwgsGivenPoly["D_dagger"]
+        self.D = kwgsGivenPoly["D"]
+        
+        # Device
+        self.device = kwgsGivenPoly["device"]
+        
+        # Multi-GPU data parallel
+        self.W = nn.DataParallel(self.W)
+        self.D = nn.DataParallel(self.D)
+        self.D_dagger = nn.DataParallel(self.D_dagger)
+        
+        self.W.to(self.device)
+        self.D.to(self.device)
+        self.D_dagger.to(self.device)
+        
+    # Calculate f 
+    def calf(self, x, xDot, t):
+        # Initialize Vs
+        batch_size = x.shape[0]
+        time_steps = x.shape[1]
+        # xis[:, :, :] = 1. 
+        
+        
+        # Loop through time steps
+        
+        if self.dim_xi > 0:
+            xiNext = torch.zeros([batch_size, self.dim_xi], requires_grad=True, device=self.device)
+            # xi0 = torch.zeros([batch_size, self.dim_xi], requires_grad=True, device=self.device)
+            
+            # List of fs
+            list_fs = []
+            # list_xis = [xi0]
+            
+            for idx in range(x.shape[1]):
+                # Load xi_curr
+                xiCurr = xiNext
+
+                # f = \partial W / \partial V
+                X_D_dagger = torch.concat([xDot[:, idx:idx + 1], xiCurr], dim = 1).requires_grad_()
+                # X_W.to(self.device)
+                D_dagger = torch.sum(self.D_dagger(X_D_dagger))
+
+                this_piece = torch.autograd.grad(outputs=D_dagger, inputs=X_D_dagger, create_graph=True)[0]
+
+                # Solve for \dot{\xi} + \partial W / \partial \xi = 0
+                dD_daggerdXi = this_piece[:, 1:]
+                dD_daggerdXDot = this_piece[:, 0:1]
+
+                X_W = x[:, idx:idx+1].requires_grad_()
+                W = torch.sum(self.W(X_W))
+                dWdX = torch.autograd.grad(outputs=W, inputs=X_W, create_graph=True)[0]
+
+                list_fs.append(dD_daggerdXDot + dWdX.reshape([-1, 1]))
+
+                # XiDot = dD^*/d\dot{d} (-dD^\dagger / dXi)
+                if idx < x.shape[1] - 1:
+                    this_input = -dD_daggerdXi.clone().requires_grad_()
+                    D = torch.sum(self.D(this_input))
+                    xiNext = xiCurr + torch.autograd.grad(outputs=D, inputs=this_input, create_graph=True)[0] * (t[:, idx + 1:idx + 2] - t[:, idx:idx + 1])
+                    # list_xis.append(xiNext)
+                    
+                    del this_input, dD_daggerdXi, dD_daggerdXDot, W, X_W, dWdX, D, this_piece, X_D_dagger, D_dagger 
+                else:
+                    del dD_daggerdXi, dD_daggerdXDot, W, X_W, dWdX, this_piece, X_D_dagger, D_dagger
+                    
+            self.fs = torch.concat(list_fs, dim=1)
+            del xiNext, xiCurr
+            
+        else:
+            X_W = x.clone().reshape([x.shape[0], x.shape[1], 1]).requires_grad_()
+            # print(X_W)
+            W = torch.sum(self.W(X_W))
+
+            X_D_dagger = xDot.clone().reshape([xDot.shape[0], xDot.shape[1], 1]).requires_grad_()
+            D_dagger = torch.sum(self.D_dagger(X_D_dagger))
+            self.fs = torch.autograd.grad(outputs=W, inputs=X_W, create_graph=True)[0].reshape([x.shape[0], x.shape[1]]) \
+                      + torch.autograd.grad(outputs=D_dagger, inputs=X_D_dagger, create_graph=True)[0].reshape([xDot.shape[0], xDot.shape[1]])
+            del W, X_W, X_D_dagger, D_dagger 
+
+    # Save the model
+    def save(self, PATH):
+        # mkdir
+        Path(PATH).mkdir(parents=True, exist_ok=True)
+
+        # Save all three neural networks
+        torch.save(self, PATH + "/model.pth")
+        torch.save(self.W.module.state_dict(), PATH + "/W.pth")
+        torch.save(self.D.module.state_dict(), PATH + "/D.pth")
+        torch.save(self.D_dagger.module.state_dict(), PATH + "/D_dagger.pth")
+
+    # Load the nns
+    def load_nns(self, PATH, mapDevice):
+        self.W.module.load_state_dict(torch.load(PATH + "/W.pth", map_location=mapDevice))
+        self.D.module.load_state_dict(torch.load(PATH + "/D.pth", map_location=mapDevice))
+        self.D_dagger.module.load_state_dict(torch.load(PATH + "/D_dagger.pth", map_location=mapDevice))
+
+        # Send to devices
+        self.W.module.to(mapDevice)
+        self.D.module.to(mapDevice)
+        self.D_dagger.module.to(mapDevice)
+
 
 class PotsCalXiXiDot:
     # Initialization of W and D
@@ -601,20 +708,20 @@ def plotGenVXFric(VV, tt, t, Vs, xs, Frics):
 
     return f, axs
 
-def load_model(modelPrefix, mapDevice=torch.device("cpu"), dim_xi=1, dict_flag=False, NN_Flag = True):
+def load_model(modelPrefix, mapDevice=torch.device("cpu"), dim_xi=1, dict_flag=False, NN_Flag = 1):
     if dict_flag:
         
         PATH = "./model/" + modelPrefix + "_dimXi_" + str(dim_xi) + "_dict"
         myModel = torch.load(PATH + "/model.pth", map_location = mapDevice)
         myModel.device = mapDevice
 
-        del myModel.W, myModel.D, myModel.D_dagger
+        # del myModel.W, myModel.D, myModel.D_dagger
         
-        if NN_Flag == True:
+        if NN_Flag == 1:
             myModel.W = PP(myModel.NNs_W, input_dim = 1, output_dim = 1)
             myModel.D = PP(myModel.NNs_D, input_dim = myModel.dim_xi, output_dim = 1)
             myModel.D_dagger = PP(myModel.NNs_D_dagger, input_dim = 1 + myModel.dim_xi, output_dim = 1)
-        else:
+        elif NN_Flag == 0:
             p_order_W = myModel.W.module.ProdOrder[1]
             p_order_D = myModel.D.module.ProdOrder[1]
             p_order_D_dagger = myModel.D_dagger.module.ProdOrder[1]
@@ -624,7 +731,10 @@ def load_model(modelPrefix, mapDevice=torch.device("cpu"), dim_xi=1, dict_flag=F
                 myModel.D_dagger = PN([1 + myModel.dim_xi, p_order_D_dagger], myModel.logVFlag)
             else:
                 myModel.D_dagger = PN([1 + myModel.dim_xi, p_order_D_dagger])
-                
+        else:
+            myModel.W = W_poly()
+            myModel.D_dagger = D_dagger_poly()
+            myModel.D = Dstar_poly()
 
         # Multi-GPU data parallel
         myModel.W = nn.DataParallel(myModel.W)
@@ -635,6 +745,9 @@ def load_model(modelPrefix, mapDevice=torch.device("cpu"), dim_xi=1, dict_flag=F
         myModel.D.module.load_state_dict(torch.load(PATH + "/D.pth", map_location=mapDevice))
         myModel.D_dagger.module.load_state_dict(torch.load(PATH + "/D_dagger.pth", map_location=mapDevice))
 
+        # Load
+        # print("myModel.W.module.coef: ", myModel.W.module.coef)
+
         # Send to devices
         myModel.W = myModel.W.module.to(mapDevice)
         myModel.D = myModel.D.module.to(mapDevice)
@@ -643,3 +756,56 @@ def load_model(modelPrefix, mapDevice=torch.device("cpu"), dim_xi=1, dict_flag=F
         myModel = torch.load("./model/" + modelPrefix + "_dimXi_" + str(dim_xi) + ".pth", map_location = mapDevice)
         myModel.device = mapDevice
     return myModel
+
+    # Make functions of W, D_dagger, Dstar for pytorch
+class W_poly(nn.Module):
+    def __init__(self, intercept = torch.tensor(0.), coef = torch.zeros(3)):
+        super().__init__()
+        self.intercept = nn.Parameter(intercept)
+        self.coef = nn.Parameter(coef.reshape([-1, 1]))
+    
+    def forward(self, x):
+        Xin = torch.concat([x, x ** 2, x ** 3], dim = 1)
+        res = torch.matmul(Xin, self.coef) + self.intercept
+        return res
+
+
+class D_dagger_poly(nn.Module):
+    def __init__(self, intercept = torch.tensor(0.), coef = torch.zeros(13)):
+        super().__init__()
+        self.intercept = nn.Parameter(intercept)
+        self.coef = nn.Parameter(coef.reshape([-1, 1]))
+    
+    def forward(self, x):
+        Vs = x[:, :1]
+        xis = x[:, 1:]
+        Xin = torch.concat([Vs, 
+                    Vs ** 2, 
+                    xis, 
+                    xis ** 2, 
+                    torch.log(Vs), 
+                    torch.log(Vs) ** 2, 
+                    Vs * xis, 
+                    torch.log(Vs) * Vs, 
+                    torch.log(Vs) * xis, 
+                    torch.log(Vs) * (Vs ** 2), 
+                    (torch.log(Vs) ** 2) * Vs, 
+                    torch.log(Vs) * (xis ** 2), 
+                    (torch.log(Vs) ** 2) * xis], dim = 1)
+        # print("Xin.shape: ", Xin.shape)
+        # print("self.coef: ", self.coef.shape)
+        # print("self.intercept: ", self.intercept.shape)
+
+        res = torch.matmul(Xin, self.coef) + self.intercept
+        return res
+
+class Dstar_poly(nn.Module):
+    def __init__(self, intercept = torch.tensor(0.), coef = torch.zeros(4)):
+        super().__init__()
+        self.intercept = nn.Parameter(intercept)
+        self.coef = nn.Parameter(coef.reshape([-1, 1]))
+    
+    def forward(self, x):
+        Xin = torch.concat([x, x ** 2, x ** 3, x ** 4], dim = 1)
+        res = torch.matmul(Xin, self.coef) + self.intercept
+        return res
